5c2d7610fc0524142055d394fa8af6699a475296
Andrian_Gaol <114342396+andriangaol@users.noreply.github.com>
2025-08-07 20:02:48 +0700

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,8 @@ from telegram.ext import Application, MessageHandler, CommandHandler, filters
 from routing import handle_message
 import os
 from dotenv import load_dotenv
+from flask import Flask
+from threading import Thread
 
 # Load environment variables
 load_dotenv()
@@ -33,6 +35,21 @@ async def error_handler(update, context):
     if update.message:
         await update.message.reply_text(error_message)
 
+def run_flask():
+    app = Flask('')
+
+    @app.route('/')
+    def home():
+        return "Bot is alive!"
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run_flask)
+    t.start()
+
+
 def main():
     """Start the bot."""
     # Ambil token dari environment variable
@@ -52,6 +69,7 @@ def main():
     application.add_error_handler(error_handler)
 
     # Mulai bot
+    keep_alive()
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
